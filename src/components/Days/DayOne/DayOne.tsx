1c079cae6af7893123ca778ca11fd19e0421ce41
Joe Ekiert <jekiert@draftkings.com>
2023-12-29 16:17:16 -0500

    part one is done

--- src/components/Days/DayOne/DayOne.tsx
+++ src/components/Days/DayOne/DayOne.tsx
@@ -1,10 +1,37 @@
 import * as React from "react";
+import { useState } from "react";
+
+import { partOneHandler } from "./script";
 
 export const DayOne = () => {
+  const [data, setData] = useState("");
+  const [dayOneResult, setDayOneResult] = useState<number>(0);
+
+  const handlePartOneClick = () => {
+    const result = partOneHandler(data);
+    setDayOneResult(result);
+  };
+
   return (
     <div className="App">
       <h1>Hello Day One</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <div>
+        <textarea
+          cols={20}
+          rows={20}
+          onChange={(e) => setData(e.target.value)}
+        />
+      </div>
+      <div>
+        <button onClick={handlePartOneClick} disabled={data.length === 0}>
+          Run Part One
+        </button>
+      </div>
+
+      <div>
+        <input type="text" value={dayOneResult} readOnly />
+      </div>
     </div>
   );
 };
