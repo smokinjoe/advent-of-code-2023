b953e8f70a411eaf1776ad3c82f788cb335e16f7
Joe <joe.ekiert@gmail.com>
2023-12-29 16:17:41 +0000

    Trying to get wouter to client-side link with rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,71 @@
+import { Route, Link, Switch, Router, useRoute } from "wouter";
+import makeCachedMatcher from "wouter/matcher";
+import { pathToRegexp, Key } from "path-to-regexp";
+
+import { DayOne } from "./components/Days/DayOne/DayOne";
+
 import "./styles.css";
 
-export default function App() {
+/*
+ * This function specifies how strings like /app/:users/:items* are
+ * transformed into regular expressions.
+ *
+ * Note that it is just a wrapper around `pathToRegexp`, which uses a
+ * slighly different convetion of returning the array of keys.
+ *
+ * @param {string} path — a path like "/:foo/:bar"
+ * @return {{ keys: [], regexp: RegExp }}
+ */
+const convertPathToRegexp = (path: string): { keys: Key[]; regexp: RegExp } => {
+  const keys: Key[] = [];
+
+  // we use original pathToRegexp package here with keys
+  const regexp = pathToRegexp(path, keys);
+  return { keys, regexp };
+};
+
+// signature of the matcher fn: (pattern, path) => [success, params]
+const customMatcher = makeCachedMatcher(convertPathToRegexp);
+
+const ActiveLink = (props: { href: string; children: React.ReactNode }) => {
+  const [isActive] = useRoute(props.href);
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Link {...props}>
+      <a href="/" className={isActive ? "active" : ""}>
+        {props.children}
+      </a>
+    </Link>
   );
-}
+};
+
+const App = () => {
+  return (
+    <Router matcher={customMatcher}>
+      <div className="App">
+        <nav>
+          <a href="/">Home</a>
+          <a href="/day-one">Day One</a>
+          <ActiveLink href="/">Home</ActiveLink>
+          <ActiveLink href="/day-one">Day One</ActiveLink>
+        </nav>
+
+        <main>
+          <Switch>
+            <Route path="/">Welcome!</Route>
+
+            <Route path="/day-one">
+              <DayOne />
+            </Route>
+            <Route path="/:anything*">
+              <center>
+                <b>404:</b> {`Sorry, this page isn't ready yet!`}
+              </center>
+            </Route>
+          </Switch>
+        </main>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/components/Days/DayOne/DayOne.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+export const DayOne = () => {
+  return (
+    <div className="App">
+      <h1>Hello Day One</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+};
