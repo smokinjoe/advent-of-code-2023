1c211dc6ee823634277440836fe19a554c020e42
Joe Ekiert <jekiert@draftkings.com>
2023-12-30 18:20:40 -0500

    creating separate EngineSchematic class

---
+++ src/utils/stringCheckers.ts
@@ -0,0 +1,11 @@
+export const isNumber = (value: string): boolean => !isNaN(Number(value));
+// TODO: add more characters
+export const isSymbol = (value: string): boolean =>
+  value.match(/^[@#$%&*-+=//]*$/) ? true : false;
+/**
+ * Day Three specific checkers
+ */
+export const dayThree = {
+  isEngineSymbol: (value: string): boolean => !isNumber(value) && value !== ".",
+  isGear: (value: string): boolean => value === "*",
+};
