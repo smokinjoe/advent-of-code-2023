4fae1df9f6a877a5667a9deba2961248788a292c
Joe Ekiert <jekiert@draftkings.com>
2024-01-01 18:23:47 -0500

    part one example is working, but I need to not generate default map values

--- src/components/Days/DayFive/partOne.ts
+++ src/components/Days/DayFive/partOne.ts
@@ -1,1 +1,120 @@
-export const partOneHandler = (data: string) => data.length;
+import { convertMultiLineStringToArray } from "../../../utils/stringReader";
+
+const Delimiters = {
+  SeedToSoil: "seed-to-soil map:",
+  SoilToFertilizer: "soil-to-fertilizer map:",
+  FertilizerToWater: "fertilizer-to-water map:",
+  WaterToLight: "water-to-light map:",
+  LightToTemperature: "light-to-temperature map:",
+  TemperatureToHumidity: "temperature-to-humidity map:",
+  HumidityToLocation: "humidity-to-location map:",
+} as const;
+
+const delimiterToMap = {
+  [Delimiters.SeedToSoil]: "seedToSoilMap",
+  [Delimiters.SoilToFertilizer]: "soilToFertilizerMap",
+  [Delimiters.FertilizerToWater]: "fertilizerToWaterMap",
+  [Delimiters.WaterToLight]: "waterToLightMap",
+  [Delimiters.LightToTemperature]: "lightToTemperatureMap",
+  [Delimiters.TemperatureToHumidity]: "temperatureToHumidityMap",
+  [Delimiters.HumidityToLocation]: "humidityToLocationMap",
+} as const;
+
+class Garden {
+  public seeds: number[] = [];
+
+  public seedToSoilMap: Map<number, number> = new Map();
+  public soilToFertilizerMap: Map<number, number> = new Map();
+  public fertilizerToWaterMap: Map<number, number> = new Map();
+  public waterToLightMap: Map<number, number> = new Map();
+  public lightToTemperatureMap: Map<number, number> = new Map();
+  public temperatureToHumidityMap: Map<number, number> = new Map();
+  public humidityToLocationMap: Map<number, number> = new Map();
+
+  private generateMapping = (
+    map: Map<number, number>,
+    destination: number,
+    source: number,
+    length: number
+  ) => {
+    for (let i = 0; i < length; i++) {
+      map.set(source + i, destination + i);
+    }
+  };
+
+  private extractGardenMapData = (
+    // TODO: There has to be a cleaner way to do this
+    mapPhase: (typeof Delimiters)[keyof typeof Delimiters],
+    dataArray: Array<string>
+  ) => {
+    const map = this[delimiterToMap[mapPhase]];
+    while (dataArray.length > 0) {
+      if ((Object.values(Delimiters) as string[]).includes(dataArray[0])) {
+        break;
+      }
+      const line = dataArray.shift();
+      const [destinationString, sourceString, lengthString] =
+        line?.split(" ") || [];
+
+      const destination = parseInt(destinationString);
+      const source = parseInt(sourceString);
+      const length = parseInt(lengthString);
+
+      this.generateMapping(map, destination, source, length);
+    }
+  };
+
+  constructor(data: string) {
+    const dataArray = convertMultiLineStringToArray(data);
+
+    // Set default mappings
+    Object.values(delimiterToMap).forEach((map) => {
+      this.generateMapping(this[map], 0, 0, 100);
+    });
+
+    // Extract seeds by popping off initial key/value and converting to number
+    const seeds = dataArray.shift()?.replace("seeds: ", "").split(" ");
+    this.seeds = seeds?.map((seed) => parseInt(seed)) || [];
+
+    // Extract maps
+    while (dataArray.length > 0) {
+      const line = dataArray.shift();
+      if (line === Delimiters.SeedToSoil) {
+        this.extractGardenMapData(Delimiters.SeedToSoil, dataArray);
+      } else if (line === Delimiters.SoilToFertilizer) {
+        this.extractGardenMapData(Delimiters.SoilToFertilizer, dataArray);
+      } else if (line === Delimiters.FertilizerToWater) {
+        this.extractGardenMapData(Delimiters.FertilizerToWater, dataArray);
+      } else if (line === Delimiters.WaterToLight) {
+        this.extractGardenMapData(Delimiters.WaterToLight, dataArray);
+      } else if (line === Delimiters.LightToTemperature) {
+        this.extractGardenMapData(Delimiters.LightToTemperature, dataArray);
+      } else if (line === Delimiters.TemperatureToHumidity) {
+        this.extractGardenMapData(Delimiters.TemperatureToHumidity, dataArray);
+      } else if (line === Delimiters.HumidityToLocation) {
+        this.extractGardenMapData(Delimiters.HumidityToLocation, dataArray);
+      }
+    }
+  }
+
+  public get lowestLocation() {
+    const locationLengths: number[] = [];
+
+    this.seeds.forEach((seed) => {
+      // Iterate through all maps starting from seed-to-soil and ending with humidity-to-location
+      let currentValue = seed;
+      Object.values(delimiterToMap).forEach((map) => {
+        currentValue = this[map].get(currentValue) ?? -1;
+      });
+      locationLengths.push(currentValue);
+    });
+
+    // and return the lowest location
+    return Math.min(...locationLengths);
+  }
+}
+
+export const partOneHandler = (data: string) => {
+  const garden = new Garden(data);
+  return garden.lowestLocation;
+};
